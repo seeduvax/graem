002de3fd1230f4557957f9213df5b215651e0272
Sebastien Devaux <sebastien.devaux@ariane.group>
2022-11-02 18:24:21 +0100

    #71de929 issue management

---
+++ _charm/src/22303ea620153916f9ddf8f5df64fabfba4c8e22c783882b2429282697f931db.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="22303ea620153916f9ddf8f5df64fabfba4c8e22c783882b2429282697f931db" state="open">
+<title>Trajectory avatar</title>
+<reporter>m026258</reporter>
+<creation>2022-11-02 18:24:20+01:00</creation>
+<description></description>
+<links>
+<link name="parent">71de9297caa46143175b78c31fb35c325e94b21b82b57ae7b336d384ffd26cdf</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/71de9297caa46143175b78c31fb35c325e94b21b82b57ae7b336d384ffd26cdf.cr
+++ _charm/src/71de9297caa46143175b78c31fb35c325e94b21b82b57ae7b336d384ffd26cdf.cr
@@ -8,6 +8,7 @@
 <link name="parent"></link>
 <link name="child">5131f3be0f7421accc9f770e1b1c0e7cb14cf58b88a070fb4771c3cb0dc69083</link>
 <link name="child">5379c7f7ac2a2a6a50d62da0b49786fa1bb5c56f16c0b613cd1d28f6b3098067</link>
+<link name="child">22303ea620153916f9ddf8f5df64fabfba4c8e22c783882b2429282697f931db</link>
 </links>
 <cf v="null"/>
 </cr>
