e5c07a676f6c3e95f7ac2d938bd39cd5f2cf1271
Sebastien Devaux <sebastien.devaux@eduvax.net>
2022-11-05 16:15:52 +0100

    #71de929 issue management

---
+++ _charm/src/6ea11b97e6184a991d7adfc5d68c36c5bdd899080dc61af7549bc0c995e48e0e.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="6ea11b97e6184a991d7adfc5d68c36c5bdd899080dc61af7549bc0c995e48e0e" state="open">
+<title>Lua shell with history and completion</title>
+<reporter>sdevaux</reporter>
+<creation>2022-11-05 16:15:52+01:00</creation>
+<description></description>
+<links>
+<link name="parent">71de9297caa46143175b78c31fb35c325e94b21b82b57ae7b336d384ffd26cdf</link>
+</links>
+<cf v="null"/>
+</cr>
